2f20b7460e3813e3f591091120bd656cad5927c8
Meghna Pamula <mpamula25@cmc.edu>
2023-02-07 21:31:49 -0800

    attempt to fix

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,9 +1,12 @@
 #!/bin/python3
 
+import re
+
 
 def validate_html(html):
     '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
+    This function performs a limited version of html validation by
+    checking whether every opening tag has a corresponding closing tag.
 
     >>> validate_html('<strong>example</strong>')
     True
@@ -11,21 +14,51 @@ def validate_html(html):
     False
     '''
 
+    stack = []
+
+    index = 0
+    tags = _extract_tags(html)
+
+    if len(tags) == 0:
+        return False
+
+    while index < len(tags):
+        tag = tags[index]
+        if "/" not in tag:
+            stack.append(tag)
+        if "/" in tags:
+            x = stack.pop()
+            if x == tags[1:]:
+                stack.pop()
+            else:
+                return False
+
+    index += 1
+
     # HINT:
-    # use the _extract_tags function below to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced parentheses algorithm from the class/book
-    # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
+    # use the _extract_tags function below to generate
+    # a list of html tags without any extra text;
+    # then process these html tags using the balanced
+    # parentheses algorithm from the class/book
+    # the main difference between your code and the code from
+    # class will be that you will have to keep track of
+    # not just the 3 types of parentheses,
     # but arbitrary text located between the html tags
 
 
 def _extract_tags(html):
     '''
     This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
+    By convention in Python, helper functions that are not meant to
+    be used directly by the user are prefixed with an underscore.
 
-    This function returns a list of all the html tags contained in the input string,
+    This function returns a list of all the html tags
+    contained in the input string,
     stripping out all text not contained within angle brackets.
 
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+
+    tags = re.findall(r"<[^>]+>", html)
+    return tags
